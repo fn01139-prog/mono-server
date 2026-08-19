b81b3b732f81aa5203fae8384ec57fee4120c653
fn01139-prog <fn01139@gmail.com>
2026-08-19 22:35:59 +0900

    Modify config.js for new prefix and empty description
    Updated prefix and cleared description in config.js

---
+++ projects/wled-bridge/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name:        'wled itfff 연결',
+  prefix:      '/wled-bridge',
+  description: '',
+  icon:        '🏦',
+  enabled:     true,
+  spa:         true,
+  public:      true,
+};
